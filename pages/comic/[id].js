7a9d179d717f73fbdf851052a30ed8c70f4f595f
xavigu <xavigu91@gmail.com>
2022-09-20 20:02:39 +0200

    Add pagination to the comic page file

--- pages/comic/[id].js
+++ pages/comic/[id].js
@@ -1,9 +1,11 @@
 import Head from "next/head";
-import Header from "components/Header";
 import Image from "next/image";
-import fs from 'fs/promises'
+import Link from "next/link";
+import { readFile, readdir, stat } from 'fs/promises'
+import { basename } from "path";
+import Header from "components/Header";
 
-export default function Comic({ id, img, alt, title, width, height}) {
+export default function Comic({ id, img, alt, title, width, height, hasPrevious, hasNext, prevId, nextId}) {
   return <>
     <Head>
       <title>Comics for developers</title>
@@ -14,10 +16,14 @@ export default function Comic({ id, img, alt, title, width, height}) {
     <Header/>
 
     <main>
-      <section className="max-w-lg m-auto">
+      <section className="max-w-lg m-auto text-center">
         <h1 className="font-bold">{`Comic #${id} - ${title}`}</h1>
-        <Image src={img} alt={alt} width={width} height={height}/>
+        <Image layout="responsive" src={img} alt={alt} width={width} height={height}/>
         <p>{alt}</p>
+        <div className="flex justify-between p-4">
+          {hasPrevious && <Link href={`/comic/${prevId}`}><a className="text-gray-400">Previous</a></Link>}
+          {hasNext && <Link href={`/comic/${nextId}`}><a className="text-gray-400">Next</a></Link>}
+        </div>
       </section>
     </main>
   </>
@@ -25,23 +31,47 @@ export default function Comic({ id, img, alt, title, width, height}) {
 
 // get the paths where you can get the props of that path
 export async function getStaticPaths() {
+  const files = await readdir('./comics');
+
+  const paths = files.map(file => {
+    const id = basename(file, '.json');
+    return { params: {id}}
+  })
+  console.log({paths})
   return {
-    paths: [{ params: { id: '2493' } }, { params: { id: '2494' } }],
+    // paths: paths
+    paths,
     fallback: false, // can also be true or 'blocking'
   }
 }
 
 // function to get the comic props
-export async function getStaticProps({params}) {
+export async function getStaticProps({ params }) {
   const { id } = params;
-  const content = await fs.readFile(`./comics/${id}.json`, 'utf8');
+  const content = await readFile(`./comics/${id}.json`, 'utf8');
   const comic = JSON.parse(content);
+
+  const idNumber = +id;
+  const prevId = idNumber - 1;
+  const nextId = idNumber + 1;
+
+  const [prevResult, nextResult] = await Promise.allSettled([
+    stat(`./comics/${prevId}.json`),
+    stat(`./comics/${nextId}.json`)
+  ]);
+
+  const hasPrevious = prevResult.status === 'fulfilled';
+  const hasNext = nextResult.status === 'fulfilled';
 
   console.log('comic: ', comic)
   return {
     // return a copy of the object comic
     props: {
-      ...comic
+      ...comic,
+      hasPrevious,
+      hasNext,
+      prevId,
+      nextId
     }
   }
 }
