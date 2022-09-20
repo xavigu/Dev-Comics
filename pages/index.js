5242a446a8c2f9de101f7588d6facc14206e4ff3
xavigu <xavigu91@gmail.com>
2022-09-20 18:05:44 +0200

    Add height and width in the images from the data of the json files

--- pages/index.js
+++ pages/index.js
@@ -15,8 +15,8 @@ export default function Home({ latestComics }) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Header></Header>
-      <main>
-        <h2 className='text-3xl font-bold text-center mb-4'>Latest Comics</h2>
+      <main className='text-center'>
+        <h2 className='text-3xl font-bold mb-4'>Latest Comics</h2>
         <section className='grid grid-cols-1 gap-2 max-w-md m-auto sm:grid-cols-2'>
           {
             latestComics.map(comic => {
@@ -24,7 +24,7 @@ export default function Home({ latestComics }) {
                 <Link href={`/comic/${comic.id}`} key={comic.id}>
                   <a className='mb-4 pb-4'>
                     <h3 className='font-bold text-sm text-center pb-2'>{comic.title}</h3>
-                    <Image src={comic.img} alt={comic.alt} width='500' height='500' layout='intrinsic' objectFit='contain'/>
+                    <Image src={comic.img} alt={comic.alt} width={comic.width} height={comic.height} layout='intrinsic' objectFit='contain'/>
                   </a>
                 </Link>
               )
