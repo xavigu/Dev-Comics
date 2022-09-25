647630f5a88e46cc71f3eb647253bda817c7fdfe
xavigu <xavigu91@gmail.com>
2022-09-25 19:03:31 +0200

    Add an input search in Header to find comics in Algolia and a change in the service to cache the searches

--- components/Header.js
+++ components/Header.js
@@ -1,6 +1,23 @@
 import Link from "next/link";
+import { useRef, useState } from "react";
 
 export default function Header() {
+  const [results, setResults] = useState([])
+  const searchRef = useRef()
+
+  const getValue = () => searchRef.current?.value
+  
+  const handleChange = () => {
+    const q = getValue()
+  
+    if (!q) return
+
+    fetch(`/api/search?q=${q}`)
+      .then(res => res.json())
+      .then(searchResults => {
+        setResults(searchResults)
+      })
+  }
   return (
     <header className="flex justify-between p-4">
       <h1 className="font-bold">
@@ -13,7 +30,33 @@ export default function Header() {
       <nav>
         <ul className="flex flex-row gap-2">
           <li><Link href='/'><a className="text-xs font-semibold">Home</a></Link></li>
-          <li><Link href='/search'><a className="text-xs font-semibold">Search</a></Link></li>
+          <li>
+            <input className='px-4 py-1 text-xs border border-gray-400 rounded-3xl' ref={searchRef} type='search' onChange={handleChange} />
+            <div className='relative z-10'>
+            {
+              Boolean(results.length) && <div className='absolute top-0 left-0'>
+                <ul className='z-50 w-full overflow-hidden bg-white border rounded-lg shadow-xl border-gray-50'>
+                  <li className='m-0' key='all-results'>
+                    <Link href={`/search?q=${getValue()}`}>
+                      <a className='block px-2 py-1 overflow-hidden text-sm italic font-semibold text-gray-400 hover:bg-slate-200 text-ellipsis whitespace-nowrap'>Ver {results.length} resultados</a>
+                    </Link>
+                  </li>
+
+                      
+                  {results.map(result => {
+                    return (
+                      <li className='m-0' key={result.id}>
+                        <Link href={`/comic/${result.id}`}>
+                          <a className='block px-2 py-1 overflow-hidden text-sm font-semibold hover:bg-slate-200 text-ellipsis whitespace-nowrap'>{result.title}</a>
+                        </Link>
+                      </li>
+                    )
+                  }) }
+                </ul>
+              </div>
+            }
+            </div>     
+          </li>
         </ul>
       </nav>
     </header>
--- services/search.js
+++ services/search.js
@@ -3,11 +3,20 @@ import algoliasearch from 'algoliasearch/lite';
 const client = algoliasearch('KXMQLTBXDN', '5ff04310341a23f98877c2efefbc4f03');
 const index = client.initIndex('prod_comics');
 
+// cache para reducir el numero de llamadas
+const CACHE = {}
 
 export const search = async ({query}) => {
+  if (CACHE[query]) {
+    return {results: CACHE[query]}
+  }
+
   const { hits } = await index.search(query, {
     attributesToRetrieve: ['id','title','img','alt'],
     hitsPerPage: 10
   });
+
+  CACHE[query]= hits;
+
   return {results: hits}
 }
